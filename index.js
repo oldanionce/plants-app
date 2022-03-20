88289a6ff9cdc250e7a6ba0bcdab921d135ff99a
Anna Lisbona <arisubona@gmail.com>
2022-03-20 21:45:01 +0100

    First test

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+//Importing needed packages
 const createError = require("http-errors");
-//Importing express and path package
 const express = require("express");
 const path = require("path");
-
-//package to parse cookie values
 const cookieParser = require("cookie-parser");
+
 const app = express();
 
 //Method to catch not Found error
