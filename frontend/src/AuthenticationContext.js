1a014c0ef6edabf76d8e307745f8fd6d99ff106d
Ana <anionce91@gmail.com>
2022-03-22 20:53:42 +0100

    added authentication context

---
+++ frontend/src/AuthenticationContext.js
@@ -0,0 +1,54 @@
+import React, { useContext, useEffect, useState } from 'react';
+
+export const AuthenticationContext = React.createContext();
+
+const AuthenticationProvider = ({ children }) => {
+	const [authData, setAuthData] = useState({});
+
+	/* async function getLoggedUser() {
+		const response = await fetch(`/api/auth/getuser`, {
+			method: 'GET',
+			// The following also for myplants url, for authenticated urls
+			credentials: 'include',
+			headers: {
+				Accept: 'application/json',
+			},
+		});
+
+		try {
+			const data = await response.json();
+			login(data);
+		} catch {
+			return null;
+		}
+	}
+
+	useEffect(() => {
+		getLoggedUser();
+		// eslint-disable-next-line
+	}, []); */
+
+	const login = data => {
+		setAuthData({ data });
+	};
+
+	const logout = () => {
+		setAuthData('');
+	};
+
+	const value = {
+		authData,
+		login,
+		logout,
+	};
+
+	return (
+		<AuthenticationContext.Provider value={value}>{children}</AuthenticationContext.Provider>
+	);
+};
+
+export const useAuthentication = () => {
+	return useContext(AuthenticationContext);
+};
+
+export default AuthenticationProvider;
