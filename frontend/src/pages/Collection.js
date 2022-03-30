0261e581d2c55871df43c877016888201a5e24b2
Ana <anionce91@gmail.com>
2022-03-30 20:05:40 +0200

    solved collection

--- frontend/src/pages/Collection.js
+++ frontend/src/pages/Collection.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import CollectionGrid from "../components/CollectionGrid/CollectionGrid";
-import Pagination from "../components/Pagination/Pagination";
-import Navigation from "../components/Navigation/Navigation.js";
-import Search from "../components/Search/Search.js";
-import Filter from "../components/Filter/Filter.js";
-import Footer from "../components/Footer/Footer";
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import CollectionGrid from '../components/CollectionGrid/CollectionGrid';
+import Pagination from '../components/Pagination/Pagination';
+import Navigation from '../components/Navigation/Navigation.js';
+import Search from '../components/Search/Search.js';
+import Filter from '../components/Filter/Filter.js';
+import Footer from '../components/Footer/Footer';
 
 export default function Collection() {
 	const navigate = useNavigate();
 
-  const [plants, setPlants] = useState([]);
-  const [filterPlants, setFilterPlants] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [plantsPerPage] = useState(10);
-  const [input, setInput] = useState("");
-  const [isLoading, setLoading] = useState(true);
-  const [orderedAZ, setorderedAZ] = useState(true);
+	const [plants, setPlants] = useState([]);
+	const [filterPlants, setFilterPlants] = useState([]);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [plantsPerPage] = useState(10);
+	const [input, setInput] = useState('');
+	const [isLoading, setLoading] = useState(true);
+	const [orderedAZ, setorderedAZ] = useState(true);
 
 	// DATA: Fetching the Plants Collection
 	function fetchPlants() {
@@ -34,93 +34,82 @@ export default function Collection() {
 		fetchPlants();
 	}, []);
 
-  // PAGINATION: get current page of plants list
-  const indexOfLastPlant = currentPage * plantsPerPage;
-  const indexOfFirstPlant = indexOfLastPlant - plantsPerPage;
-  const currentPlants = filterPlants.slice(indexOfFirstPlant, indexOfLastPlant);
+	// console.log(plants);
+
+	// PAGINATION: get current page of plants list
+	const indexOfLastPlant = currentPage * plantsPerPage;
+	const indexOfFirstPlant = indexOfLastPlant - plantsPerPage;
+	const currentPlants = filterPlants.slice(indexOfFirstPlant, indexOfLastPlant);
+
+	// PAGINATION: change page
+	const paginate = pageNumber => setCurrentPage(pageNumber);
 
 	//Handlers
 	const handleInputChange = e => {
 		setInput(e.target.value);
 	};
 
-	const handleSubmitSearch = () => {
-		const searchedWord = input.toLowerCase().trim();
-		const newPlants = [...plants];
-		setFilterPlants(
-			newPlants.filter(plant => plant.commonName.toLowerCase().includes(searchedWord))
-		);
+	const handleSubmitSearch = event => {
+		if (event.key === 'Enter') {
+			const searchedWord = input.toLowerCase().trim();
+			const newPlants = [...plants];
+			setFilterPlants(
+				newPlants.filter(plant => plant.commonName.toLowerCase().includes(searchedWord))
+			);
+		}
 	};
 
-  const handleSubmitSearch = (event) => {
-    if (event.key === "Enter") {
-      const searchedWord = input.toLowerCase().trim();
-      const newPlants = [...plants];
-      setFilterPlants(
-        newPlants.filter((plant) =>
-          plant.commonName.toLowerCase().includes(searchedWord)
-        )
-      );
-    }
-  };
+	const handleSort = () => {
+		const sortedPlants = [...plants];
+		if (orderedAZ) {
+			setFilterPlants(
+				sortedPlants.sort((a, b) => (a.scientificName > b.scientificName ? 1 : -1))
+			);
+			setorderedAZ(false);
+		} else {
+			setFilterPlants(
+				sortedPlants.sort((a, b) => (a.scientificName > b.scientificName ? -1 : 1))
+			);
+			setorderedAZ(true);
+		}
+	};
 
-  const handleSort = () => {
-    const sortedPlants = [...plants];
-    if (orderedAZ) {
-      setFilterPlants(
-        sortedPlants.sort((a, b) =>
-          a.scientificName > b.scientificName ? 1 : -1
-        )
-      );
-      setorderedAZ(false);
-    } else {
-      setFilterPlants(
-        sortedPlants.sort((a, b) =>
-          a.scientificName > b.scientificName ? -1 : 1
-        )
-      );
-      setorderedAZ(true);
-    }
-  };
+	return (
+		<>
+			<Navigation></Navigation>
+			<header className='container plantsTitle'>
+				<div className='content'>
+					<h1>Collection</h1>
+					<Search
+						handleInputChange={handleInputChange}
+						handleSubmitSearch={handleSubmitSearch}></Search>
+					<Filter handleSort={handleSort}></Filter>
+				</div>
+			</header>
 
-  return (
-    <>
-      <Navigation></Navigation>
-      <header className="container plantsTitle">
-        <div className="content">
-          <h1>Collection</h1>
-          <Search
-            handleInputChange={handleInputChange}
-            handleSubmitSearch={handleSubmitSearch}
-          ></Search>
-          <Filter handleSort={handleSort}></Filter>
-        </div>
-      </header>
-
-      <main className="container plantsGrid">
-        <div className="content">
-          <div className="gridHeader">
-            {/* HERE: div amb el component 'filters' */}
-            <Pagination
-              plantsPerPage={plantsPerPage}
-              totalPlants={filterPlants.length}
-              paginate={paginate}
-              currentPage={currentPage}
-            />
-          </div>
-          <CollectionGrid plants={currentPlants}></CollectionGrid>
-          <div className="gridFooter">
-            <Pagination
-              plantsPerPage={plantsPerPage}
-              totalPlants={filterPlants.length}
-              paginate={paginate}
-              currentPage={currentPage}
-            />
-          </div>
-        </div>
-      </main>
-      <Footer></Footer>
-    </>
-  );
-
+			<main className='container plantsGrid'>
+				<div className='content'>
+					<div className='gridHeader'>
+						{/* HERE: div amb el component 'filters' */}
+						<Pagination
+							plantsPerPage={plantsPerPage}
+							totalPlants={filterPlants.length}
+							paginate={paginate}
+							currentPage={currentPage}
+						/>
+					</div>
+					<CollectionGrid plants={currentPlants}></CollectionGrid>
+					<div className='gridFooter'>
+						<Pagination
+							plantsPerPage={plantsPerPage}
+							totalPlants={filterPlants.length}
+							paginate={paginate}
+							currentPage={currentPage}
+						/>
+					</div>
+				</div>
+			</main>
+			<Footer></Footer>
+		</>
+	);
 }
