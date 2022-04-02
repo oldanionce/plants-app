fedeea49685fb42ab0e8d9a253c96c1b19e6b6ad
Anna Lisbona <arisubona@gmail.com>
2022-04-02 10:45:47 +0200

    Myplants part 1
    SOS

--- frontend/src/components/SaveToMyPlantsForm/SaveToMyPlantsForm.js
+++ frontend/src/components/SaveToMyPlantsForm/SaveToMyPlantsForm.js
@@ -1,15 +1,19 @@
-import './SaveToMyPlantsForm.css';
+import "./SaveToMyPlantsForm.css";
 
-export default function SaveToMyPlantsForm({}) {
-	
-	return (
-        <div class="modalForm">
-            <form>
-                <div>
-                    <input type="text" id="saveToMyPlants" name="saveToMyPlants" placeholder="Pick a nickname" />
-                    <button>Save to My Plants</button>
-                </div>
-            </form>
+export default function SaveToMyPlantsForm({ addToMyPlants }) {
+  return (
+    <div class="modalForm">
+      <form>
+        <div>
+          <input
+            type="text"
+            id="saveToMyPlants"
+            name="saveToMyPlants"
+            placeholder="Pick a nickname"
+          />
+          <button onclick={addToMyPlants}>Save to My Plants</button>
         </div>
-	);
+      </form>
+    </div>
+  );
 }
