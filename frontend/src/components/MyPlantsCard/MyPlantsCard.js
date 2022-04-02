448b4c0096650d54bf8db8782e194f3259f1364e
Ana <anionce91@gmail.com>
2022-04-02 12:28:09 +0200

    first iteration

--- frontend/src/components/MyPlantsCard/MyPlantsCard.js
+++ frontend/src/components/MyPlantsCard/MyPlantsCard.js
@@ -1,28 +1,27 @@
-import "./MyPlantsCard.css";
-import PlantCard from "../PlantCard/PlantCard";
-import { useLocation } from "react-router-dom";
+import './MyPlantsCard.css';
+import PlantCard from '../PlantCard/PlantCard';
+import { useLocation } from 'react-router-dom';
 
-export default function MyPlantsCard({
-  plant,
-  addToMyPlants,
-  deleteFromMyPlants,
-  isLoading,
-}) {
-  // Hooks
-  //	const location = useLocation();
+export default function MyPlantsCard({ plant, addToMyPlants, deleteFromMyPlants, isLoading }) {
+	// Hooks
+	//	const location = useLocation();
 
-  return (
-    <li className="myplantscard" key={plant._id}>
-      <PlantCard
-        id={plant._id}
-        url={plant.image}
-        addToMyPlants={addToMyPlants}
-        deleteFromMyPlants={deleteFromMyPlants}
-        commonName={plant.commonName}
-        interiorExterior={plant.interiorExterior}
-        careLevel={plant.careLevel}
-        petFriendly={plant.petFriendly}
-      ></PlantCard>
-    </li>
-  );
+	return (
+		<li className='myplantscard' key={plant._id}>
+			<PlantCard
+				id={plant._id}
+				imageUrl={plant.image}
+				addToMyPlants={addToMyPlants}
+				deleteFromMyPlants={deleteFromMyPlants}
+				commonName={plant.commonName}
+				interiorExterior={plant.interiorExterior}
+				careLevel={plant.careLevel}
+				irrigation={plant.irrigation}
+				irrigationSummer={plant.irrigationSummer}
+				irrigationWinter={plant.irrigationWinter}
+				location={plant.location}
+				soil={plant.soil}
+				petFriendly={plant.petFriendly}></PlantCard>
+		</li>
+	);
 }
