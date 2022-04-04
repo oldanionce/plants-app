7eb85c69e34a40da3157cc7d27530951eeed057b
Anna Lisbona <arisubona@gmail.com>
2022-04-04 20:00:25 +0200

    PetFriendly Filter
    PetFriendly filter function done

--- frontend/src/components/Filter/Filter.js
+++ frontend/src/components/Filter/Filter.js
@@ -1,31 +1,29 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowDownAZ } from '@fortawesome/free-solid-svg-icons'
-import { faPaw } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDownAZ } from "@fortawesome/free-solid-svg-icons";
+import { faPaw } from "@fortawesome/free-solid-svg-icons";
 
 import "./Filter.css";
 
-const sortIcon = <FontAwesomeIcon icon={faArrowDownAZ} />
-const petFriendlyIcon = <FontAwesomeIcon icon={faPaw} />
+const sortIcon = <FontAwesomeIcon icon={faArrowDownAZ} />;
+const petFriendlyIcon = <FontAwesomeIcon icon={faPaw} />;
 
-
-
-export default function Filter({ handleSort }) {
+export default function Filter({ handleSort, handlePetFriendly }) {
   return (
     <div class="filters">
       {/* Care Level selector */}
       <select name="carelevel" className="carelevel">
-				<option value="0">Dificultad:</option>
-				<option value="1">Fácil</option>
-				<option value="2">Medio</option>
-				<option value="3">Difícil</option>
-			</select>
+        <option value="0">Dificultad:</option>
+        <option value="1">Fácil</option>
+        <option value="2">Medio</option>
+        <option value="3">Difícil</option>
+      </select>
 
       {/* Pet Friendly Plants Only */}
-			<button className="petfriendly-button">
+      <button className="petfriendly-button" onClick={handlePetFriendly}>
         {petFriendlyIcon}
       </button>
-          
+
       {/* Alphabetical Order */}
       <button className="order-button" onClick={handleSort}>
         {sortIcon}
