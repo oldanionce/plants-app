adab79aa1da9fd83255b3812d37b2554460e761d
Ana <anionce91@gmail.com>
2022-04-06 18:38:46 +0200

    added temperature to modal

--- frontend/src/components/CollectionItem/CollectionItem.js
+++ frontend/src/components/CollectionItem/CollectionItem.js
@@ -8,10 +8,10 @@ import './CollectionItem.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const closeIcon = <FontAwesomeIcon icon={faXmark} />;
-const addIcon = <FontAwesomeIcon icon={faPlus} />
+const addIcon = <FontAwesomeIcon icon={faPlus} />;
 
 const customStyles = {
 	content: {
@@ -54,53 +54,57 @@ export default function CollectionItem({
 
 	return (
 		<>
-		<li className='collectionCard' key={plant._id} onClick={openModal}>
-			<CollectionCard
-				id={plant._id}
-				imageUrl={plant.image}
-				commonName={plant.commonName}
-				interiorExterior={plant.interiorExterior}
-				careLevel={plant.careLevel}
-				petFriendly={plant.petFriendly}
-				addToMyPlants={addToMyPlants}
-				deleteFromMyPlants={deleteFromMyPlants}
-				// only needed for Collection
-				scientificName={plant.scientificName}
-				location={plant.location}
-				irrigation={plant.irrigation}
-				irrigationSummer={plant.irrigationSummer}
-				irrigationWinter={plant.irrigationWinter}
-				soil={plant.soil}
-				temperature={plant.temperature}
-				// only needed for myPlants
-				nickname={plant.nickname}></CollectionCard>
-				<span className="addIcon" key={plant._id}>{addIcon}</span>
-		</li>
-		<Modal
-			isOpen={modalIsOpen}
-			onRequestClose={closeModal}
-			style={customStyles}
-			contentLabel={plant.scientificName}>
-			<button className='modalCloseModal' key={plant._id} onClick={closeModal}>{closeIcon}</button>
-			<CollectionModal
-				id={plant._id}
-				imageUrl={plant.image}
-				commonName={plant.commonName}
-				interiorExterior={plant.interiorExterior}
-				careLevel={plant.careLevel}
-				petFriendly={plant.petFriendly}
-				addToMyPlants={addToMyPlants}
-				deleteFromMyPlants={deleteFromMyPlants}
-				scientificName={plant.scientificName}
-				location={plant.location}
-				irrigation={plant.irrigation}
-				irrigationSummer={plant.irrigationSummer}
-				irrigationWinter={plant.irrigationWinter}
-				soil={plant.soil}
-				handleNicknameChange={handleNicknameChange}
-				nickname={nickname}>
-			</CollectionModal>
-		</Modal>
+			<li className='collectionCard' key={plant._id} onClick={openModal}>
+				<CollectionCard
+					id={plant._id}
+					imageUrl={plant.image}
+					commonName={plant.commonName}
+					interiorExterior={plant.interiorExterior}
+					careLevel={plant.careLevel}
+					petFriendly={plant.petFriendly}
+					addToMyPlants={addToMyPlants}
+					deleteFromMyPlants={deleteFromMyPlants}
+					// only needed for Collection
+					scientificName={plant.scientificName}
+					location={plant.location}
+					irrigation={plant.irrigation}
+					irrigationSummer={plant.irrigationSummer}
+					irrigationWinter={plant.irrigationWinter}
+					soil={plant.soil}
+					temperature={plant.temperature}
+					// only needed for myPlants
+					nickname={plant.nickname}></CollectionCard>
+				<span className='addIcon' key={plant._id}>
+					{addIcon}
+				</span>
+			</li>
+			<Modal
+				isOpen={modalIsOpen}
+				onRequestClose={closeModal}
+				style={customStyles}
+				contentLabel={plant.scientificName}>
+				<button className='modalCloseModal' key={plant._id} onClick={closeModal}>
+					{closeIcon}
+				</button>
+				<CollectionModal
+					id={plant._id}
+					imageUrl={plant.image}
+					commonName={plant.commonName}
+					interiorExterior={plant.interiorExterior}
+					careLevel={plant.careLevel}
+					petFriendly={plant.petFriendly}
+					addToMyPlants={addToMyPlants}
+					deleteFromMyPlants={deleteFromMyPlants}
+					scientificName={plant.scientificName}
+					location={plant.location}
+					irrigation={plant.irrigation}
+					irrigationSummer={plant.irrigationSummer}
+					irrigationWinter={plant.irrigationWinter}
+					temperature={plant.temperature}
+					soil={plant.soil}
+					handleNicknameChange={handleNicknameChange}
+					nickname={nickname}></CollectionModal>
+			</Modal>
 		</>
 	);
 }
