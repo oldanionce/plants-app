d78c53af05da0ebd684f044fe0dddc971bf9496e
Anna Lisbona <arisubona@gmail.com>
2022-04-06 19:32:07 +0200

    Calendar_part1

--- frontend/src/components/Event/Event.js
+++ frontend/src/components/Event/Event.js
@@ -4,26 +4,29 @@ import { atcb_init } from "add-to-calendar-button";
 const Atcb_action = () => {
   React.useEffect(atcb_init, []);
   return (
-    <div className="atcb" style={{ display: "none" }}>
-      {JSON.stringify({
-        name: "Add the title of your event",
-        startDate: "2022-01-14",
-        endDate: "2022-01-18",
-        startTime: "10:15",
-        endTime: "23:30",
-        options: [
-          "Apple",
-          "Google",
-          "iCal",
-          "Microsoft365",
-          "Outlook.com",
-          "MicrosoftTeams",
-          "Yahoo",
-        ],
-        trigger: "click",
-        iCalFileName: "Reminder-Event",
-      })}
-    </div>
+    <button className="atcb_customTrigger" onClick={Atcb_action}>
+      Añadir recordatorio
+      <div className="atcb" style={{ display: "none" }}>
+        {JSON.stringify({
+          name: "Añade el título del evento",
+          startDate: "2022-01-14",
+          endDate: "2022-01-18",
+          startTime: "10:15",
+          endTime: "23:30",
+          options: [
+            "Apple",
+            "Google",
+            "iCal",
+            "Microsoft365",
+            "Outlook.com",
+            "MicrosoftTeams",
+            "Yahoo",
+          ],
+          trigger: "click",
+          iCalFileName: "Reminder-Event",
+        })}
+      </div>{" "}
+    </button>
   );
 };
 
