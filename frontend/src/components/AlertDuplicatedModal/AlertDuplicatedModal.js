749ad18f63e9b25834190bf0d7308678514a27be
Ana <anionce91@gmail.com>
2022-04-06 21:49:43 +0200

    added input validation

---
+++ frontend/src/components/AlertDuplicatedModal/AlertDuplicatedModal.js
@@ -0,0 +1,13 @@
+import './AlertDuplicatedModal.css';
+
+export default function AlertDuplicatedModal() {
+	return (
+		<>
+			<div class='modalContent'>
+				<div class='modalAlert'>
+					<h2>You already have a plant with that nickname!</h2>
+				</div>
+			</div>
+		</>
+	);
+}
---
+++ frontend/src/components/AlertEmptyModal/AlertEmptyModal.js
@@ -0,0 +1,13 @@
+import './AlertEmptyModal.css';
+
+export default function AlertEmptyModal() {
+	return (
+		<>
+			<div class='modalContent'>
+				<div class='modalAlert'>
+					<h2>You need to enter a nickname!</h2>
+				</div>
+			</div>
+		</>
+	);
+}
