96e091e90d7ea5eb518606b323f737e7fa322cee
Anna Lisbona <arisubona@gmail.com>
2022-04-09 09:39:16 +0200

    Calendar description irrigation
    Irrigation function calendar.

--- frontend/src/components/Event/Event.js
+++ frontend/src/components/Event/Event.js
@@ -11,54 +11,51 @@ const Atcb_action = ({
   React.useEffect(atcb_init, []);
   let dateObj = new Date();
   let month = dateObj.getUTCMonth();
-  let day = dateObj.getUTCDay();
+  let day = dateObj.getDate();
   let year = dateObj.getUTCFullYear();
   let hour = dateObj.getUTCHours();
   let minutes = dateObj.getUTCMinutes();
-  let newDate = `${year}-${month}-${day}`;
-  // let currentHour = `${hour}:${minutes}`;
-  // const irrigationInSummer = ({ irrigationSummer }) => {
-  //   let newValue= ""
-  //   if (irrigationSummer === 1) {
-  //     newValue = "riega una vez al mes"
-  //   if (irrigationSummer === 2) {
-  //     newValue = "riega una vez por semana"
-
-  //   } if (irrigationSummer === 3) {
-  //     newValue = "riega 2/3 veces a la semana"
-
-  //   }
-  //   if (irrigationSummer === 4) {
-  //     newValue = "riega a diario"
-
-  //   }
-  //   return newValue
-
-  // };
-  // const irrigationInWinter = ({ irrigationWinter }) => {
-  //   let newValue= ""
-  //   if (irrigationWinter === 1) {
-  //     newValue = "riega una vez al mes"
-  //   if (irrigationWinter === 2) {
-  //     newValue = "riega una vez por semana"
-
-  //   } if (irrigationWinter === 3) {
-  //     newValue = "riega 2/3 veces a la semana"
-
-  //   }
-  //   if (irrigationWinter === 4) {
-  //     newValue = "riega a diario"
-
-  //   }
-  //   return newValue
-
-  // };
+  let newDate = `${year}-${month + 1}-${day}`;
+  console.log(newDate);
+  // // let currentHour = `${hour}:${minutes}`;
+  const irrigationInSummer = () => {
+    let newValue = "";
+    if (irrigationSummer === 1) {
+      newValue = "riega una vez al mes";
+    }
+    if (irrigationSummer === 2) {
+      newValue = "riega una vez por semana";
+    }
+    if (irrigationSummer === 3) {
+      newValue = "riega 2/3 veces a la semana";
+    }
+    if (irrigationSummer === 4) {
+      newValue = "riega a diario";
+    }
+    return newValue;
+  };
+  const irrigationInWinter = () => {
+    let newValue = "";
+    if (irrigationWinter === 1) {
+      newValue = "riega una vez al mes";
+    }
+    if (irrigationWinter === 2) {
+      newValue = "riega una vez por semana";
+    }
+    if (irrigationWinter === 3) {
+      newValue = "riega 2/3 veces a la semana";
+    }
+    if (irrigationWinter === 4) {
+      newValue = "riega a diario";
+    }
+    return newValue;
+  };
   return (
     <div className="atcb">
       {JSON.stringify({
         label: "Añadir recordatorio",
         name: `Regar ${nickname}`,
-        description: `¡Recuerda! ${irrigation}. En verano ${irrigationSummer}. En invierno ${irrigationWinter}`,
+        description: `¡Recuerda! ${irrigation}. En verano ${irrigationInSummer()}. En invierno ${irrigationInWinter()}`,
         startDate: `${newDate}`,
         endDate: `${newDate}`,
         startTime: "10:15",
